4f6e9f624ea85854a46b060d7a538b7f85f5c91a
najahyuga <najahyugad@gmail.com>
2023-01-24 17:06:15 +0700

    Add folder Lib and file Lib/MyFunction and file RequireDanInclude

---
+++ PHP/PHP-Dasar/Lib/MyFunction.php
@@ -0,0 +1,4 @@
+<?php
+function sayHello(string $first_name, string $last_name){
+    return "Hello $first_name $last_name";
+}
---
+++ PHP/PHP-Dasar/RequireDanInclude.php
@@ -0,0 +1,5 @@
+<?php
+require_once "Lib/MyFunction.php";
+// include_once "Lib/MyFunction.php";
+
+echo sayHello("najah", "yuga");
