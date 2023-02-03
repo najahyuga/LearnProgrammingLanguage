378068c1ffce46976d8bb54a72c704f1dfd76a3b
najahyuga <najahyugad@gmail.com>
2023-02-03 21:58:22 +0700

    Update source code in BusinessLogic/RemoveToDoList.php

--- PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/BusinessLogic/RemoveToDoList.php
+++ PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/BusinessLogic/RemoveToDoList.php
@@ -2,6 +2,19 @@
 /**
  * remove Todo di List
  */
-function removeToDoList(){
+function removeTodoList(int $number):bool
+{
+    global $todolist;
 
+    if ($number > sizeof($todolist)) {
+        return false;
+    }
+
+    for ($i = $number; $i < sizeof($todolist) ; $i++) { 
+        $todolist[$i] = $todolist[$i + 1];
+    }
+
+    unset($todolist[sizeof($todolist)]);
+
+    return true;
 }
