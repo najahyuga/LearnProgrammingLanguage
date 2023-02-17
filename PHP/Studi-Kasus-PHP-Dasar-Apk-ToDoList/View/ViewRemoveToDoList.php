4a055f7716f9e4389f685518937369cbb3727533
najahyuga <najahyugad@gmail.com>
2023-02-17 13:36:03 +0700

    Update source code in View/ViewRemoveToDoList.php

--- PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/View/ViewRemoveToDoList.php
+++ PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/View/ViewRemoveToDoList.php
@@ -1,5 +1,20 @@
 <?php
+require_once __DIR__ . "/../Helper/Input.php";
+require_once __DIR__ . "/../BusinessLogic/RemoveToDoList.php";
 
 function viewRemoveTodoList(){
-    
+    echo "Menghapus Todo" . PHP_EOL;
+
+    $pilihan = input("Nomor (x untuk batalkan)");
+
+    if ($pilihan == "x"){
+        echo "Batal Menghapus Todo" . PHP_EOL;
+    }else{
+        $success = removeTodoList($pilihan);
+        if ($success){
+            echo "Sukses menghapus todo nomor $pilihan" . PHP_EOL;
+        }else{
+            echo "Gagal menghapus todo nomor $pilihan" . PHP_EOL;
+        }
+    }
 }
