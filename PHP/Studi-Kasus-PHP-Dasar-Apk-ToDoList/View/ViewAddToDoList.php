c1196e716741018d17d282a440f96cc4938f074f
najahyuga <najahyugad@gmail.com>
2023-02-17 13:35:39 +0700

    Update source code in View/ViewAddToDoList.php

--- PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/View/ViewAddToDoList.php
+++ PHP/Studi-Kasus-PHP-Dasar-Apk-ToDoList/View/ViewAddToDoList.php
@@ -1,5 +1,16 @@
 <?php
+require_once __DIR__ . "/../Model/ToDoList.php";
+require_once __DIR__ . "/../BusinessLogic/AddToDoList.php";
+require_once __DIR__ . "/../Helper/Input.php";
 
 function viewAddTodoList(){
-    
+    echo "Menambah Todo" . PHP_EOL;
+
+    $todo = input("Todo (x untuk batal)");
+
+    if($todo == "x"){
+        echo "Batal Menambah Todo" . PHP_EOL;
+    }else{
+        addTodoList($todo);
+    }
 }
