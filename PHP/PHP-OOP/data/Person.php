2d68ed0b57688c655e5119838ef012b1d7560354
najahyuga <najahyugad@gmail.com>
2023-02-22 23:58:42 +0700

    Update source code and add oop properties

--- PHP/PHP-OOP/data/Person.php
+++ PHP/PHP-OOP/data/Person.php
@@ -1,3 +1,10 @@
 <?php
 class Person{
+
+    //Properties dengan kata kunci var
+    //Properties dengan type declaration setelah kata kunci var, diikuti dengan tipe data
+    
+    var string $name;
+    var ?string $address = null;//Properties dengan nullable properties dengan menambahkan tanda ?
+    var string $country = "Indonesia";//Properties dengan default value
 }
