a09a500917178e0c56e4d2990bc584bfce9df841
najahyuga <najahyugad@gmail.com>
2023-02-28 22:52:12 +0700

    Update source code and add new OOP Self Keyword

--- PHP/PHP-OOP/Function.php
+++ PHP/PHP-OOP/Function.php
@@ -2,7 +2,10 @@
 require_once "data/Person.php";
 
 //Object dengan kata kunci new
-$person1 = new Person();
+$person1 = new Person("yuga", "surabaya");
 $person1->name = "najah";//untuk di akses this keyword
 
 $person1->sayHello(null);//memanggil function
+
+//memanggil constant di dalam class menggunakan function(self keyword)
+$person1->info();
