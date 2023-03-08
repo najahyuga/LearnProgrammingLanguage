4a593b9dc8af1c459ea65cb0e79000649dc14b1e
najahyuga <najahyugad@gmail.com>
2023-03-08 13:19:20 +0700

    Add new file Helper.php and OOP Namespace

---
+++ PHP/PHP-OOP/data/Helper.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Namespace OOP adalah tempat untuk menampung/menyimpan class-class yang sama
+ * Function dan Constant di namespace cara penggunaannya di awali nama namespacenya
+ */
+namespace Helper;
+
+function helpMe(){
+    echo "Helpme" . PHP_EOL;
+}
+
+const APP = "Belajar PHP OOP";
