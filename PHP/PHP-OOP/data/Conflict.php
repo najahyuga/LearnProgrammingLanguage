2a6e7460f325251753a630f095ac6df7f14abd82
najahyuga <najahyugad@gmail.com>
2023-03-08 13:19:01 +0700

    Add new file Conflict.php and OOP Namespace

---
+++ PHP/PHP-OOP/data/Conflict.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Namespace OOP adalah tempat untuk menampung/menyimpan class-class yang sama
+ * Namespace Data\One{} ini merupakan penamaan untuk,
+ * membedakan class yang sama dalam satu file
+ */
+namespace Data\One{
+    class Conflict{
+        
+    }
+}
+
+/**
+ * Namespace OOP adalah tempat untuk menampung/menyimpan class-class yang sama
+ * Namespace Data\Two{} ini merupakan penamaan untuk,
+ *  membedakan class yang sama dalam satu file
+ */
+namespace Data\Two{
+    class Conflict{
+        
+    }
+}
