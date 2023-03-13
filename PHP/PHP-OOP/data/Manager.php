81036180553479829a6b7ba5ca4b4f6fbc78b2e9
najahyuga <najahyugad@gmail.com>
2023-03-13 13:22:58 +0700

    Update source code and Add OOP Constructor Overriding

--- PHP/PHP-OOP/data/Manager.php
+++ PHP/PHP-OOP/data/Manager.php
@@ -6,6 +6,17 @@
  */
 class Manager{
     var string $name;
+    var string $title;
+
+    /**
+     * OOP Constructor Overriding bisa meng-override dengan mengubah argumennya,
+     * namun disarankan untuk memanggil parent constructor
+     */
+    function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
 
     function sayHello(string $name) :void{
         echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
@@ -13,6 +24,15 @@ class Manager{
 }
 
 class VicePresident extends Manager{
+
+    /**
+     * OOP Constructor Overriding
+     */
+    function __construct(string $name = "")
+    {
+        parent::__construct($name, "VP");// Tidak wajib, Namun di Rekomendasikan
+    }
+
     function sayHello(string $name) :void{
         echo "Hi $name, my name is VP $this->name" . PHP_EOL;
     }
